2882329755c4a4b91128f43941d7b4107a4b1a36
[dyad] <sevenalter7@gmail.com>
2025-10-03 16:33:20 +0300

    [dyad] Debugging React.Children.only error by simplifying Layout - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,19 +2,20 @@
 
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-import { Toaster as Sonner } from "@/components/ui/sonner"; // Re-added import
+// Temporarily remove Header, Footer, and Sonner to isolate the error
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
 
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
+      {/* <Header /> */}
+      <main className="flex-1 p-4"> {/* Added padding for visibility */}
         <Outlet />
       </main>
-      <Footer />
-      <Sonner /> {/* Re-added Toaster component */}
+      {/* <Footer /> */}
+      {/* <Sonner /> */}
     </div>
   );
 };
