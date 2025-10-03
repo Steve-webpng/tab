38e63ecad95962e32623cf7b74d8f3714dc6af04
[dyad] <sevenalter7@gmail.com>
2025-10-04 00:05:46 +0300

    [dyad] Re-introduced Footer and Sonner components to complete the layout and enable toast notifications. - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,19 +2,19 @@
 
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./Header"; // Re-introducing Header
-// import Footer from "./Footer";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
+import Header from "./Header";
+import Footer from "./Footer"; // Uncommenting Footer import
+import { Toaster as Sonner } from "@/components/ui/sonner"; // Uncommenting Sonner import
 
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header /> {/* Re-introducing Header */}
+      <Header />
       <main className="flex-1 p-4">
         <Outlet />
       </main>
-      {/* <Footer /> */}
-      {/* <Sonner /> */}
+      <Footer /> {/* Re-introducing Footer */}
+      <Sonner /> {/* Re-introducing Sonner */}
     </div>
   );
 };
