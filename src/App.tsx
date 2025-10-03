cb185997a34c4223042ea9570d5df74bef77f236
[dyad] <sevenalter7@gmail.com>
2025-10-03 16:15:29 +0300

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -15,28 +13,24 @@ import Layout from "./components/layout/Layout";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <> {/* Using a React Fragment to wrap multiple top-level components */}
-    <Toaster />
-    <Sonner />
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="marketplace" element={<MarketplacePage />} />
-              <Route path="marketplace/:id" element={<ProductDetailPage />} />
-              <Route path="gigs" element={<GigsPage />} />
-              <Route path="gigs/:id" element={<GigDetailPage />} />
-              <Route path="create-listing" element={<CreateListingPage />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="marketplace" element={<MarketplacePage />} />
+            <Route path="marketplace/:id" element={<ProductDetailPage />} />
+            <Route path="gigs" element={<GigsPage />} />
+            <Route path="gigs/:id" element={<GigDetailPage />} />
+            <Route path="create-listing" element={<CreateListingPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Toaster } from "@/components/ui/toaster"; // Import Toaster here
+import { Toaster as Sonner } from "@/components/ui/sonner"; // Import Sonner here
 
 const Layout = () => {
   return (
@@ -13,6 +15,8 @@ const Layout = () => {
         <Outlet />
       </main>
       <Footer />
+      <Toaster /> {/* Render Toaster here */}
+      <Sonner /> {/* Render Sonner here */}
     </div>
   );
 };
