57ed869d2e706e97e72f8b5c17948ba7d2dcd227
[dyad] <sevenalter7@gmail.com>
2025-10-03 16:17:35 +0300

    [dyad] wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,19 +2,21 @@
 
 import React from "react";
 import { Outlet } from "react-router-dom";
-// Temporarily remove Header, Footer, Toaster, Sonner imports for debugging
-// import Header from "./Header";
-// import Footer from "./Footer";
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      {/* Temporarily removed Header, Footer, Toaster, Sonner */}
+      <Header />
       <main className="flex-1">
         <Outlet />
       </main>
+      <Footer />
+      <Toaster />
+      <Sonner />
     </div>
   );
 };
