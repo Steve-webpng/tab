4c9d033a21d8708c1791616fef7fbc6a2e8d2088
[dyad] <sevenalter7@gmail.com>
2025-10-03 23:56:34 +0300

    [dyad] Re-introducing Header component to debug - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,16 +2,15 @@
 
 import React from "react";
 import { Outlet } from "react-router-dom";
-// Temporarily remove Header, Footer, and Sonner to isolate the error
-// import Header from "./Header";
+import Header from "./Header"; // Re-introducing Header
 // import Footer from "./Footer";
 // import { Toaster as Sonner } from "@/components/ui/sonner";
 
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      {/* <Header /> */}
-      <main className="flex-1 p-4"> {/* Added padding for visibility */}
+      <Header /> {/* Re-introducing Header */}
+      <main className="flex-1 p-4">
         <Outlet />
       </main>
       {/* <Footer /> */}
